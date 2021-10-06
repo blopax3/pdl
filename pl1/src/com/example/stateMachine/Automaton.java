63c90231da01502ae1932fa348d6a3284dfd13fe
Pablo Sánchez Gallego <pablo.sanchezgallego@edu.uah.es>
2021-10-06 13:04:38 +0200

    a1

--- pl1/src/com/example/stateMachine/Automaton.java
+++ pl1/src/com/example/stateMachine/Automaton.java
@@ -36,44 +36,49 @@ public class Automaton {
     }
 
     public void loadMatrix() {
+        // rehacer
         matrix.get(0).put('a', 1);
         matrix.get(1).put('a', 2);
         matrix.get(1).put('b', 3);
         matrix.get(2).put('a', 2);
         matrix.get(2).put('b', 3);
-        matrix.get(3).put('c', 5);
-        matrix.get(3).put('d', 8);
-        matrix.get(3).put('m', 7);
-        matrix.get(3).put('n', 6);
-        matrix.get(3).put('o', 4);
-        matrix.get(3).put('q', 9);
+        matrix.get(3).put('c', 7);
+        matrix.get(3).put('m', 8);
+        matrix.get(3).put('n', 4);
+        matrix.get(3).put('o', 6);
+        matrix.get(3).put('p', 9);
+        matrix.get(3).put('q', 5);
         matrix.get(4).put('b', 10);
-        matrix.get(5).put('c', 5);
-        matrix.get(5).put('m', 7);
-        matrix.get(5).put('o', 7);
-        matrix.get(5).put('p', 8);
-        matrix.get(5).put('q', 9);
+        matrix.get(5).put('b', 10);
         matrix.get(6).put('b', 10);
-        matrix.get(7).put('b', 10);
+        matrix.get(7).put('c', 7);
+        matrix.get(7).put('m', 8);
+        matrix.get(7).put('n', 4);
+        matrix.get(7).put('o', 6);
+        matrix.get(7).put('p', 9);
+        matrix.get(7).put('q', 5);
         matrix.get(8).put('b', 10);
         matrix.get(9).put('b', 10);
-        matrix.get(10).put('c', 12);
-        matrix.get(10).put('m', 14);
-        matrix.get(10).put('n', 13);
-        matrix.get(10).put('o', 11);
+        matrix.get(10).put('c', 14);
+        matrix.get(10).put('m', 15);
+        matrix.get(10).put('n', 11);
+        matrix.get(10).put('o', 13);
         matrix.get(10).put('p', 15);
-        matrix.get(10).put('q', 16);
+        matrix.get(10).put('q', 12);
+        matrix.get(10).put('ñ', 17);
         matrix.get(11).put('b', 10);
-        matrix.get(12).put('c', 12);
-        matrix.get(12).put('m', 14);
-        matrix.get(12).put('n', 13);
-        matrix.get(12).put('o', 11);
-        matrix.get(12).put('p', 15);
-        matrix.get(12).put('q', 16);
+        matrix.get(12).put('b', 10);
         matrix.get(13).put('b', 10);
-        matrix.get(14).put('b', 10);
+        matrix.get(14).put('c', 14);
+        matrix.get(14).put('m', 15);
+        matrix.get(14).put('n', 11);
+        matrix.get(14).put('ñ', 17);
+        matrix.get(14).put('o', 13);
+        matrix.get(14).put('p', 16);
+        matrix.get(14).put('q', 12);
         matrix.get(15).put('b', 10);
         matrix.get(16).put('b', 10);
+        matrix.get(17).put('b', 10);
 
 
     }
@@ -86,7 +91,7 @@ public class Automaton {
     }
 
     public void loadStates() {
-        for (int i=0; i < 17; i++) {
+        for (int i=0; i < 18; i++) {
             states.add(i);
         }
     }
@@ -101,14 +106,16 @@ public class Automaton {
 
     public void setFinalStates() {
         finalStates.add(4);
+        finalStates.add(5);
         finalStates.add(6);
         finalStates.add(8);
         finalStates.add(9);
         finalStates.add(11);
+        finalStates.add(12);
         finalStates.add(13);
-        finalStates.add(14);
         finalStates.add(15);
         finalStates.add(16);
+        finalStates.add(17);
     }
 
     public Integer getNextState(Integer state, Character character) {
--- pl1/src/com/example/stateMachine/Main.java
+++ pl1/src/com/example/stateMachine/Main.java
@@ -3,8 +3,8 @@ package com.example.stateMachine;
 public class Main {
 
     public static void main(String[] args) {
-        //a(!+a)*(bc*(m+n+o+p+q))(!+bc*(m+n+o+p+q))*
-        StateMachine st = new StateMachine("aabcobcpbmbqbcn");
+        //a(!+a)*(bc*(m+n+o+p+q))(!+(bc*(m+n+ñ+o+p+q)))*
+        StateMachine st = new StateMachine("aabcñ");
         System.out.println(st.checkString());
     }
 }
